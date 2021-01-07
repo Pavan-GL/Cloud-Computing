667123ffffaf8e84935eeca2bb49d71504aaedbc
pavan <pavangl222@gmail.com>
2021-01-07 09:38:20 -0800

    User module

---
+++ user/sessionclass.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+/**
+ *
+ * @author Esha_1
+ */
+public class sessionclass {
+    public static String user=null;
+    public void setusername(String name)
+    {
+        user=name;
+    }
+    public String getusername()
+    {
+        return user;
+    }
+    
+}
