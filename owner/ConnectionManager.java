33cbaa2f4d53d90d451b5592306ff72565bdc6d5
pavan <pavangl222@gmail.com>
2021-01-07 09:27:43 -0800

    Admin module

---
+++ owner/ConnectionManager.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package owner;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Esha_1
+ */
+public class ConnectionManager {
+    private static String url="jdbc:mysql://localhost:3306/College";
+    private static String driverName="com.mysql.jdbc.Driver";
+    private static String username="root";
+    private static String password="root";
+    private static Connection con;
+    
+    public static Connection getConnection()
+    {
+        try
+        {
+            Class.forName(driverName);
+            try
+            {
+                con=DriverManager.getConnection(url,username,password);
+            }
+            catch(SQLException ex)
+            {
+                System.out.println("Failed to create the database connection");
+            }
+        }
+        catch(ClassNotFoundException ex)
+        {
+            System.out.println("Driver Not Found");
+        }
+        return con;
+    }
+    
+            
+        
+    
+    
+}
---
+++ owner/date.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package owner;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
+/**
+ *
+ * @author Esha_1
+ */
+public class date {
+    public static void main(String args[])
+    {
+        java.util.Date d=new java.util.Date();
+        String todaydate=d.toString();
+        System.out.println("today date is:"+todaydate);
+    }
+    
+}
